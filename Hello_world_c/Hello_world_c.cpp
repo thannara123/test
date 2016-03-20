fc7e6be3a258f192dd044730bf7b1662cad33f4b
unknown <thannara123@gmail.com>
2016-03-20 21:29:40 +0530

    first meesage

---
+++ Hello_world_c/Hello_world_c.cpp
@@ -0,0 +1,17 @@
+// Hello_world_c.cpp : Defines the entry point for the console application.
+//
+
+#include <stdio.h>
+#include <conio.h>
+
+
+
+int main()
+{
+	printf("Hello World");
+	
+	_getch();
+	return 0;
+
+}
+
